6c5814087829d93de3d1d640bd0fa74e16b92e5e
Ranthony S <rmulo.mayancela.1976@gmail.com>
2025-04-28 20:05:37 -0500

    inicio aplicacion

---
+++ resources/views/componets/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    <a href="/">Inicio</a> 
+    <a href="/notas">Notas</a> 
+    <a href="/matricula">Matriculas</a>
+    <a href="/contactos">Contactos</a>
+    </nav>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/notas', function () {
+    return view('notas');
+});
+
+Route::get('/matricula', function () {
+    return view('matricula');
+});
+
+Route::get('/contactos', function () {
+    return view('contactos');
+});
